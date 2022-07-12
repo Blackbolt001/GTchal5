269c90a90a2959b1ac33a8ac98e3e537513e2718
unknown <Jgossett73@gmail.com>
2022-07-11 20:17:53 -0400

    adding js

--- assets/JavaScript/scripts.js
+++ assets/JavaScript/scripts.js
@@ -1,2 +1,14 @@
+var currentDay = moment().format("dddd,MMMM Do");
 
+var hour9 = moment().hour(9);
+var hour10 = moment().hour(10);
+var hour11 = moment().hour(11);
+var hour12 = moment().hour(12);
+var hour13 = moment().hour(13);
+var hour14 = moment().hour(14);
+var hour15 = moment().hour(15);
+var hour16 = moment().hour(16);
+var hour17 = moment().hour(17);
+var hour18 = moment().hour(18);
 
+var hours = [hour9]
